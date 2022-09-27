90565eb8bf88dbf69fe818b428f19e41928a7d87
Rolph Recto <rolph.recto@gmail.com>
2022-09-27 09:29:39 -0400

    first draft of normalizer in python

--- runtime/normalizer.py
+++ runtime/normalizer.py
@@ -5,47 +5,50 @@ OP_MUL = "mul"
 
 # EXPRESSIONS
 
+class ExpressionNode:
+    pass
+
 ## for(i, e)
-class ForNode:
+class ForNode(ExpressionNode):
     def __init__(self, index, extent, expr):
         self.index = index
         self.extent = extent
         self.expr = expr
 
     def __str__(self):
-        return "for {} in {} { {} }".format(self.index, self.extent, self.expr)
+        return "for {} in {} {{ {} }}".format(self.index, self.extent, self.expr)
 
 ## reduce(op, e)
-class ReduceNode:
+class ReduceNode(ExpressionNode):
     def __init__(self, expr, op=OP_ADD):
         self.expr = expr
         self.op = op
 
     def __str__(self):
-        reduce_str = "sum" if op == OP_ADD else "product"
+        reduce_str = "sum" if self.op == OP_ADD else "product"
         return "{}({})".format(reduce_str, self.expr)
 
 ## e op e
-class OpNode:
+class OpNode(ExpressionNode):
     def __init__(self, expr1, expr2, op=OP_ADD):
         self.expr1 = expr1
         self.expr2 = expr2
         self.op = op
 
     def __str__(self):
-        op_str = "+" if op == OP_ADD else "*"
+        op_str = "+" if self.op == OP_ADD else "*"
         return "({} {} {})".format(self.expr1, op_str, self.expr2)
 
 
 ## x[i]
-class IndexNode:
+class IndexNode(ExpressionNode):
     def __init__(self, var, index_list):
         self.var = var
         self.index_list = index_list
 
     def __str__(self):
         if len(self.index_list) > 0:
-            index_str = ["[{}]".format(ind) for ind in index_list].join("")
+            index_str = "".join(["[{}]".format(ind) for ind in self.index_list])
             return "{}{}".format(self.var, index_str)
 
         else:
@@ -68,10 +71,10 @@ class TransposeNode:
         self.perm = perm
 
     def __str__(self):
-        return "transpose({}, {}".format(self.arr, self.perm)
+        return "transpose({}, {})".format(self.arr, self.perm)
 
 
-def normalize(expr, path=[]):
+def normalize(expr: ExpressionNode, path=[]):
     if isinstance(expr, ForNode):
         return normalize(expr.expr, path + [("index", (expr.index, expr.extent))])
 
@@ -81,7 +84,7 @@ def normalize(expr, path=[]):
 
     elif isinstance(expr, OpNode):
         new_expr1 = normalize(expr.expr1, path)
-        new_expr2 = normalize(expr.expr1, path)
+        new_expr2 = normalize(expr.expr2, path)
         return OpNode(new_expr1, new_expr2, expr.op)
 
     # TODO for now, assume index nodes are scalar (0-dim)
@@ -107,9 +110,9 @@ def normalize(expr, path=[]):
                 fill_sizes.append(extent)
 
         # - second, compute transpositions
-        transpose_perm = []
+        transpose_perm = list(range(len(new_shape)))
         for i in range(len(orig_shape)):
-            transpose_perm[i] = orig_shape.index(new_shape[i])
+            transpose_perm[i] = orig_shape.index(new_shape[i][0])
 
         if len(fill_sizes) > 0:
             return TransposeNode(FillNode(IndexNode(expr.var, []), fill_sizes), transpose_perm)
@@ -119,3 +122,20 @@ def normalize(expr, path=[]):
     else:
         assert(False, "normalize: failed to match expression")
 
+if __name__ == "__main__":
+    expr = ForNode("i", 2, \
+        ReduceNode( \
+            ForNode("k", 2, \
+                OpNode( \
+                    IndexNode("A", ["i", "k"]), \
+                    IndexNode("v", ["k"]), \
+                    op = OP_MUL
+                )
+            )
+        )
+    )
+
+    norm_expr = normalize(expr)
+
+    print(expr)
+    print(norm_expr)
